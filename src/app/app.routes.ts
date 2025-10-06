89e6c606cafc49c5bb70781fcbfe2fbaf8bd05b3
vltir <87820976+vltir@users.noreply.github.com>
2025-10-06 14:15:09 +0200

    Project Refactoring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {Main} from './pages/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Main},
+];
